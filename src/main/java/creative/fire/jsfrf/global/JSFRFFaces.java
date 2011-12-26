96a8dad359f51ea4313dac6afc38428b6e02300b
feuyeux <feuyeux@gmail.com>
2011-12-26 18:51:31 +0800

    add global path

--- src/main/java/creative/fire/jsfrf/global/JSFRFFaces.java
+++ src/main/java/creative/fire/jsfrf/global/JSFRFFaces.java
@@ -6,9 +6,38 @@ import javax.servlet.http.HttpSession;
 
 public class JSFRFFaces {
 	public static String ERROR = "jsfrf_error";
+	private String cssFilePath;
+	private String imageFilePath;
+	private String jsFilePath;
+
 	public static HttpSession getSession() {
 		final ExternalContext externalContext = FacesContext.getCurrentInstance().getExternalContext();
 		HttpSession session = (HttpSession) externalContext.getSession(true);
 		return session;
+	}
+
+	public String getContextPath() {
+		final ExternalContext externalContext = FacesContext.getCurrentInstance().getExternalContext();
+		return externalContext.getRequestContextPath();
+	}
+
+	public String getCssFilePath() {
+		if (cssFilePath == null) {
+			cssFilePath = getContextPath() + "/resources/css/";
+		}
+		return cssFilePath;
+	}
+
+	public String getImagePath() {
+		if (imageFilePath == null) {
+			imageFilePath = getContextPath() + "/resources/images/";
+		}
+		return imageFilePath;
+	}
+
+	public String getJsFilePath() {
+		if (jsFilePath == null)
+			jsFilePath = getContextPath() + "/resources/javascript/";
+		return jsFilePath;
 	}
 }
