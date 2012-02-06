18f8f70d5490db11eac0b6d3554175664e6cc84f
feuyeux <feuyeux@gmail.com>
2012-02-06 18:47:48 +0800

    auto

--- src/main/webapp/resources/javascript/jsf.ajax.js
+++ src/main/webapp/resources/javascript/jsf.ajax.js
@@ -4,36 +4,35 @@ jsfrfEventCallback = function(data) {
 		var source = data.source;
 		var xml = data.responseXML;
 		var text = data.responseText;
-		alert(source.id + "\n" + status + "\n" + text);
+		alert(source.id + "\n" + status + "\n" + text + "\n" + xml.URL);
 	}
 }
 
 jsf.ajax.addOnEvent(jsfrfEventCallback);
 
-
 jsfrfErrorCallback = function(data) {
-		var componentId = data.source.id;
-		var xml = data.responseXML;
-		var text = data.responseText;
-		var code = data.responseCode;
-		var type = data.type;
-		var status = data.status;
+	var componentId = data.source.id;
+	var xml = data.responseXML;
+	var text = data.responseText;
+	var code = data.responseCode;
+	var type = data.type;
+	var status = data.status;
 
-		var description = data.description;
-		var errorName = data.errorName;
-		var errorMessage = data.errorMessage;
+	var description = data.description;
+	var errorName = data.errorName;
+	var errorMessage = data.errorMessage;
 
-		error = "errorName=" + errorName + " errorMessage=" + errorMessage
-				+ " description=" + description + " status=" + status;
-		if (status == "httpError") {
-		} else if (status == "emptyResponse") {
-		} else if (status == "malformedXML") {
-		} else if (status == "serverError") {
-		} else {
-			error = status;
-		}
-
-		alert(error);
+	error = "Ajax Error:\nstatus=" + status + "\nerrorName=" + errorName
+			+ "\nerrorMessage=" + errorMessage + "\ndescription=" + description;
+	if (status == "httpError") {
+	} else if (status == "emptyResponse") {
+	} else if (status == "malformedXML") {
+	} else if (status == "serverError") {
+	} else {
+		error = status;
 	}
+
+	alert(error);
+}
 
 jsf.ajax.addOnError(jsfrfErrorCallback);
