0929e602c47497db075bd8c56fe41f889900eac0
Eric Han <feuyeux@gmail.com>
2012-04-20 01:08:03 +0800

    auto

--- src/main/java/creative/fire/jsfrf/jsf/download/PDFDownLoad.java
+++ src/main/java/creative/fire/jsfrf/jsf/download/PDFDownLoad.java
@@ -12,7 +12,6 @@ import javax.servlet.ServletOutputStream;
 import javax.servlet.http.HttpServletResponse;
 
 import com.lowagie.text.Document;
-import com.lowagie.text.DocumentException;
 import com.lowagie.text.Element;
 import com.lowagie.text.Font;
 import com.lowagie.text.Paragraph;
@@ -30,41 +29,24 @@ public class PDFDownLoad {
 	public void generatePDF(String head, String body) throws Exception {
 		pdfFileName = "jsfrf_" + System.currentTimeMillis() + ".pdf";
 		BufferedOutputStream output = new BufferedOutputStream(new FileOutputStream(prefixDir + pdfFileName));
-		generatePDFReport(head, body, output);
-		output.close();
-		pushDownload(pdfFileName);
-	}
-
-	private void generatePDFReport(String head, String body, BufferedOutputStream output) throws DocumentException {
 		Document document = new Document();
 		PdfWriter.getInstance(document, output);
 		document.open();
 		document.newPage();
 		Font headerFont = new Font();
-		Paragraph text = new Paragraph(head, headerFont);
-		text.add("--标题");
-		document.add(text);
-		Paragraph titleText = new Paragraph(body);
-		titleText.setAlignment(Element.ALIGN_CENTER);
-		titleText.setSpacingAfter(5);
-		titleText.add("--内容");
-		document.add(titleText);
+		headerFont.setSize(28);
+		Paragraph headText = new Paragraph(head, headerFont);
+		headText.setSpacingAfter(5);
+		headText.setAlignment(Element.ALIGN_CENTER);
+		document.add(headText);
+		Paragraph contentText = new Paragraph();
+		for (int i = 0; i < 33; i++) {
+			contentText.add(i + ". " + body+"\n");
+		}
+		document.add(contentText);
 		document.close();
-	}
-
-	private HttpServletResponse obtainResponse(String fileName) {
-		FacesContext facesContext = FacesContext.getCurrentInstance();
-		HttpServletResponse response = (HttpServletResponse) facesContext.getExternalContext().getResponse();
-		String contentType = "application/pdf";
-		response.setContentType(contentType);
-
-		StringBuilder contentDisposition = new StringBuilder();
-		contentDisposition.append("attachment;");
-		contentDisposition.append("filename=\"");
-		contentDisposition.append(fileName);
-		contentDisposition.append("\"");
-		response.setHeader("Content-Disposition", contentDisposition.toString());
-		return response;
+		output.close();
+		pushDownload(pdfFileName);
 	}
 
 	private void pushDownload(String pdfFileName) {
@@ -83,5 +65,20 @@ public class PDFDownLoad {
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
+	}
+
+	private HttpServletResponse obtainResponse(String fileName) {
+		FacesContext facesContext = FacesContext.getCurrentInstance();
+		HttpServletResponse response = (HttpServletResponse) facesContext.getExternalContext().getResponse();
+		String contentType = "application/pdf";
+		response.setContentType(contentType);
+
+		StringBuilder contentDisposition = new StringBuilder();
+		contentDisposition.append("attachment;");
+		contentDisposition.append("filename=\"");
+		contentDisposition.append(fileName);
+		contentDisposition.append("\"");
+		response.setHeader("Content-Disposition", contentDisposition.toString());
+		return response;
 	}
 }
